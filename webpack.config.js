401c50252a88f7921958e88059b6f56fb3c25fc7
yasmany1998 <yasmany1998@gmail.com>
2020-01-26 00:01:23 +0100

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js', '.json' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html',
+			inject: 'body'
+    })
+  ],
+  devServer: {
+		historyApiFallback: {
+			index: "",
+			verbose: true,
+			disableDotRule: true
+    },
+     host: '0.0.0.0',
+     disableHostCheck: true
+	}
+};
